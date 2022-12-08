5d64601ecf019fba13e72ffba70fcdf60f3cf768
kozdavaa <50880843+kozdavaa@users.noreply.github.com>
2022-12-08 09:10:18 +0100

    New simmulation and result

--- matlab/postproc_trafo.m
+++ matlab/postproc_trafo.m
@@ -1,20 +1,19 @@
-% p, t, e, u exportalva
-
-r_tri = pdeintrp(p,t,p(1,:)'); % haromszog kozeppontok r koordinataja
-
-rAphi_tri = pdeintrp(p,t,u); % haromszog kozeppontokban rAphi 
-
-area = pdetrg(p,t);
-
-
-N = 2000; % menetszam
-F = 0.05*0.02; %[m2] tekercs keresztmetszet
-w = 2*pi*500;
-Jphi = 1/(0.05*0.02);
-
-indCoil = find(t(4,:) == 1); %terkercs tartomanya
-
-Psi = 2*N/F*sum( 2*pi*rAphi_tri(indCoil).*area(indCoil) );
-U = (1i*w*Psi);
-I = Jphi *(F/N);
-Z0 = U/I %[mH]
+function Z = postproc_trafo(p,t,e,u)
+       
+    rAphi_tri = pdeintrp(p,t,u); % haromszog kozeppontokban rAphi 
+    
+    area = pdetrg(p,t);
+    
+    
+    N = 2000; % menetszam
+    F = 0.05*0.02; %[m2] tekercs keresztmetszet
+    w = 2*pi*500;
+    Jphi = (1*2000)/(0.05*0.02);
+    
+    indCoil = find(t(4,:) == 1); %terkercs tartomanya
+    
+    Psi = 2*N/F*sum( 2*pi*rAphi_tri(indCoil).*area(indCoil) );
+    U = (1i*w*Psi);
+    I = Jphi *(F/N);
+    Z = U/I;
+end
